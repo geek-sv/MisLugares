0cac169720f0abdd998cffcc90b6019bb1dfc8da
Martin Espinal <marlex.espinal@gmail.com>
2014-03-27 20:52:04 -0600

    Primer commit Creación del repositorio

---
+++ MisLugares/src/com/example/mislugares/GeoPunto.java
@@ -0,0 +1,30 @@
+package com.example.mislugares;
+
+public class GeoPunto {
+
+	double longitud, latitud;
+	
+	public GeoPunto(double latitud, double longitud) {
+		// TODO Auto-generated constructor stub
+		this.latitud=(int) (latitud*1E6);
+		this.longitud= (int)(longitud*1E6);
+	}
+	
+	public String toString(){
+		return new String ("lat:"+latitud+" long"+longitud);
+	}
+	
+	public double distancia(GeoPunto punto){
+		final double RADIO_TIERRA=6378000;//en metros
+		double dLat=Math.toRadians(punto.latitud-latitud);
+		double dLong=Math.toRadians(punto.longitud-longitud);
+		double a = Math.sin(dLat/2)*
+					Math.sin(dLat/2)+
+					Math.sin(dLong/2)*
+					Math.sin(dLong/2)*
+					Math.cos(Math.toRadians(punto.latitud))*
+					Math.cos(Math.toRadians(latitud));
+		double c=2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		return RADIO_TIERRA*c;
+	}
+}
---
+++ MisLugares/src/com/example/mislugares/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.mislugares;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		case R.id.config:
+			lanzarPreferencias(null);
+			break;
+		}
+		return true;
+	}
+	
+	public void lanzarAcercaDe(View view){
+		Intent i= new Intent(this,AcercaDe.class);
+		startActivity(i);
+	}
+	
+	public void lanzarPreferencias (View view){
+		Intent i = new Intent(this, Preferencias.class);
+		startActivity(i);
+	}
+	
+	public void mostrarPreferencias(View view){
+		SharedPreferences pref=PreferenceManager.getDefaultSharedPreferences(this);
+		String s="notificaciones: "+pref.getBoolean("notificaciones", true)+",distancia mínima: "
+				+pref.getString("distancia", "?");
+		Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+	}
+	public void salir(View view){
+		finish();
+	}
+
+}
